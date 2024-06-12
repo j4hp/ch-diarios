600a992e6eeaf9bfcc26b6b37eba25a223c870e7
j4hp <artemiohuespe@gmail.com>
2024-06-12 10:00:18 -0400

    Add files via upload

---
+++ challenge_dia2.py
@@ -0,0 +1,8 @@
+numero=int(input("intruducir un numero")) 
+def tabla_multiplicar():
+    for i in range(11):
+        resultado=i*numero
+        print(f"{i} x {numero}= {resultado}")
+        
+        
+tabla_multiplicar()
