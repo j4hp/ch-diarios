9cd7be7d5f7e5dfd8a5026c0dec6ae5334cac7d8
j4hp <artemiohuespe@gmail.com>
2024-06-13 09:25:17 -0400

    Add files via upload

---
+++ challenge_dia1.py
@@ -0,0 +1,8 @@
+def invertir_cadena(cadena):
+    return cadena[::-1]
+
+cadena=str(input("ingresar una cadena de caracteres: "))
+cadena_invertida=invertir_cadena(cadena)
+
+
+print(cadena_invertida)
